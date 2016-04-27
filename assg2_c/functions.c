5bc92ff8268102670b8ab9480db2351909e68fec
mangi07 <mangi07@users.noreply.github.com>
2016-04-27 17:59:12 -0600

    Reorganizing into single source file.

--- assg2_c/functions.c
+++ assg2_c/functions.c
@@ -6,11 +6,39 @@
 #include <fcntl.h>
 #include <string.h>
 
-#include "functions.h"
 
-//int randInRange(int min, int max);
-//int isInArray(int arrElement, int arr[], int size);
-//void initRoom(int index, int connections);
+void setDirectory(char **name); 
+int makeRooms(char* allRooms[], int ALL_ROOMS_SIZE, char* directory);
+int randInRange(int min, int max); 
+void chooseRooms( char* allRooms[], int ALL_ROOMS_SIZE, char* usedRooms[], int USED_ROOMS_SIZE );
+int isInArray(char *room, char * rooms[], int size); 
+void makeRoomFiles( char* rooms[], char* directory ); 
+int strcat_safe(char *destination, int destCapacity, char *pSrc);
+void initRoom(int room_index, char* rooms[], int connections, char* directory );
+int countConnections( FILE *file);
+int makeConnection( int first, int second, char* rooms[], char* directory ); 
+int connectionExists( FILE *file_1, FILE *file_2, char *room_1, char *room_2 ); 
+void addRoomTypes( char* rooms[], char* directory ); 
+
+
+ 
+int main(void)
+{
+	char* rooms[] = {"blue", "red", "ONE", "TWO", "Big", "Small", "best", "worst", "CARPET", "WOOD"};
+	int SIZE = 10;
+	
+	char *directory_name;
+	setDirectory( &directory_name );
+
+	makeRooms( rooms, SIZE, directory_name );
+	
+	free(directory_name);
+	
+	printf("In tester...end of tests.\n");
+
+	exit(0);
+}
+
 
 
 /***************************************
@@ -29,29 +57,7 @@ void setDirectory(char **name) {
 	mkdir( *name, ACCESSPERMS );
 	//printf( "%s\n", *name );
 }
-	
 
-
-/**********************************************************************
- * Assigns a subset of allRooms to usedRooms.
- * Rooms are not assigned if allRooms size is less than usedRooms size.
- *********************************************************************/
-void chooseRooms( char* allRooms[], int ALL_ROOMS_SIZE, char* usedRooms[], int USED_ROOMS_SIZE ) {
-	if ( ALL_ROOMS_SIZE < USED_ROOMS_SIZE ) {
-		printf( "in chooseRooms: ALL_ROOMS_SIZE must be at least 7\n" );
-		exit(1);
-	}
-
-	char *chosen_room = allRooms[randInRange(0, ALL_ROOMS_SIZE - 1)];
-	int count = 0;
-	for ( count; count < USED_ROOMS_SIZE; ++count ) {
-		while (isInArray(chosen_room, usedRooms, USED_ROOMS_SIZE - 1)) {
-			 chosen_room = allRooms[randInRange(0, ALL_ROOMS_SIZE)];
-		}
-		usedRooms[count] = chosen_room;
-		//printf( "usedRooms[%d]: %s\n", count, chosen_room );
-	}
-}
 
 /**************************************************************
  * Returns 0 on success and -1 if not enough rooms are provided.
@@ -89,13 +95,35 @@ int makeRooms(char* allRooms[], int ALL_ROOMS_SIZE, char* directory) {
 }
 
 
-
 /******************************************************************************
  * Preconditions: Please seed with srand(time(0)) before calling this function.
  *****************************************************************************/
 int randInRange(int min, int max) {
 	int num = ( rand() % (max - (min - 1)) ) + min;
 	return num;
+}
+
+
+/**********************************************************************
+ * Assigns a subset of allRooms to usedRooms.
+ * Rooms are not assigned if allRooms size is less than usedRooms size.
+ *********************************************************************/
+void chooseRooms( char* allRooms[], int ALL_ROOMS_SIZE, char* usedRooms[], int USED_ROOMS_SIZE ) {
+	if ( ALL_ROOMS_SIZE < USED_ROOMS_SIZE ) {
+		printf( "in chooseRooms: ALL_ROOMS_SIZE must be at least 7\n" );
+		exit(1);
+	}
+
+	char *chosen_room = allRooms[randInRange(0, ALL_ROOMS_SIZE - 1)];
+	int count = 0;
+	for ( count; count < USED_ROOMS_SIZE; ++count ) {
+		while (isInArray(chosen_room, usedRooms, USED_ROOMS_SIZE - 1)) {
+			 chosen_room = allRooms[randInRange(0, ALL_ROOMS_SIZE)];
+		}
+		usedRooms[count] = chosen_room;
+		//printf( "usedRooms[%d]: %s\n", count, chosen_room );
+	}
+
 }
 
 
@@ -107,6 +135,100 @@ int isInArray(char *room, char * rooms[], int size) {
 		}
 	}
 	return 0;
+}
+
+
+/******************************************************
+ * Call this function at the beginning to create all
+ * room files and add the first line to each file.
+ *****************************************************/
+void makeRoomFiles( char* rooms[], char* directory ) {
+
+
+	char room_dir[100] = "";
+	FILE *file_p = NULL;
+	
+	int i;
+	for ( i = 0; i < 7; ++i ) {
+		// prepare directory string
+		strcat_safe( room_dir, 100, directory );
+		strcat_safe( room_dir, 100, "/" );
+		strcat_safe( room_dir, 100, rooms[i] );
+
+		// do initial writes to file
+		file_p = fopen( room_dir, "a+" );
+		fprintf( file_p, "ROOM NAME: %s\n", rooms[i] );
+		fclose( file_p );
+
+		memset( &room_dir[0], 0, sizeof(room_dir) );
+	}
+}
+
+
+/*************************************************************************************
+ * Inspired by https://randomascii.wordpress.com/2013/04/03/stopusingstrncpyalready/ 
+ * Returns 1 on error or 0 on success, 1 if destination char array is too small to 
+ * contain pSrc char array.
+ ************************************************************************************/
+int strcat_safe(char *destination, int destCapacity, char *pSrc) {
+
+	if ( ! ( destination && pSrc ) ) {
+		printf( "in strcat_safe: null string(s) passed in!\n" );
+		exit( 1 );
+	}
+	//printf("%d\n", strlen( pSrc ) );
+
+	int capacityNeeded = strlen(destination) + strlen(pSrc) + 1; // add one for null character
+
+
+	//printf("in strcat_safe...strlen(destination) = %d \n", strlen(destination));
+	//printf("in strcat_safe...capacityNeeded = %d \n", capacityNeeded);
+
+	if (destCapacity < capacityNeeded) {
+		return 1;
+	}
+
+	strcat(destination, pSrc);
+	// ensure null termination
+	destination[strlen(destination)] = 0;
+	
+	return 0;
+}
+
+
+/************************************************************************
+ * int room_index	Identifies the room to add connection(s) to.
+ * char* rooms[]	Array of room names, each to correspond to a file.
+ * int connections	Total number of connections the room 
+ * 			identified by room_index should have.
+ ***********************************************************************/
+void initRoom(int room_index, char* rooms[], int connections, char* directory ) {
+	char *room_name = rooms[room_index];
+	FILE *file_p = NULL;
+	// need to open in directory, so craft the string here
+	char room_dir[100];
+	strcat_safe( room_dir, 100, directory );
+	strcat_safe( room_dir, 100, "/" );
+	strcat_safe( room_dir, 100, rooms[room_index] );
+
+	file_p = fopen( room_dir, "a+" );
+	// Check for successful file opening.
+	if ( ! file_p ) {
+		printf( "Error opening file in function initRoom!\n" );
+		exit( 1 );
+	}
+
+	int connections_in_file = countConnections( file_p );
+	int connections_to_make = connections - connections_in_file;
+
+	while ( connections_to_make > 0 ) {
+		int rand_index = randInRange( 0, 7 );
+		if ( makeConnection( room_index, rand_index, rooms, directory ) ) {
+			connections_to_make--;
+		}
+	}
+
+	fclose(file_p);
 }
 
 
@@ -133,45 +255,6 @@ int countConnections( FILE *file){
 }
 
 
-// finish
-int connectionExists( FILE *file_1, FILE *file_2, char *room_1, char *room_2 ) {
-	// move both file pointers to the beginning
-	rewind( file_1 );
-	rewind( file_2 );
-
-	int first_check = 0;
-	int second_check = 0;
-
-	// check first file for second room name
-	char other_1[100] = "";
-	char name_1[100] = "";
-	while( fgets( file_1, "%s: %s\n", other_1, name_1 ) != NULL ) {
-		if ( ! cmpstr( name_1, room_2 ) ) {
-			first_check = 1;
-			break;
-		}
-	}
-
-	// check second file for first room name
-	char other_2[100] = "";
-	char name_2[100] = "";
-	while( fgets( file_2, "%s: %s\n", other_2, name_2 ) != NULL ) {
-		if ( ! cmpstr( name_2, room_1 ) ) {
-			second_check = 1;
-			break;
-		}
-	}
-
-	// return the file pointers to end position
-	lseek( file_1, 0, SEEK_END );
-	lseek( file_2, 0, SEEK_END );
-
-	// a connection exists only if both files indicate the connection
-	return ( first_check && second_check );
-}
-
-
-// finish
 /******************************************************************************
  * Checks to see whether the two rooms identified by index are the same,
  * and returns 0 if they are (avoiding a room making a connection to itself).
@@ -236,66 +319,40 @@ int makeConnection( int first, int second, char* rooms[], char* directory ) {
 }
 
 
-/************************************************************************
- * int room_index	Identifies the room to add connection(s) to.
- * char* rooms[]	Array of room names, each to correspond to a file.
- * int connections	Total number of connections the room 
- * 			identified by room_index should have.
- ***********************************************************************/
-void initRoom(int room_index, char* rooms[], int connections, char* directory ) {
-	char *room_name = rooms[room_index];
-	FILE *file_p = NULL;
-	// need to open in directory, so craft the string here
-	char room_dir[100];
-	strcat_safe( room_dir, 100, directory );
-	strcat_safe( room_dir, 100, "/" );
-	strcat_safe( room_dir, 100, rooms[room_index] );
+int connectionExists( FILE *file_1, FILE *file_2, char *room_1, char *room_2 ) {
+	// move both file pointers to the beginning
+	rewind( file_1 );
+	rewind( file_2 );
 
-	file_p = fopen( room_dir, "a+" );
-	// Check for successful file opening.
-	if ( ! file_p ) {
-		printf( "Error opening file in function initRoom!\n" );
-		exit( 1 );
-	}
+	int first_check = 0;
+	int second_check = 0;
 
-	int connections_in_file = countConnections( file_p );
-	int connections_to_make = connections - connections_in_file;
-
-	while ( connections_to_make > 0 ) {
-		int rand_index = randInRange( 0, 7 );
-		if ( makeConnection( room_index, rand_index, rooms, directory ) ) {
-			connections_to_make--;
+	// check first file for second room name
+	char other_1[100] = "";
+	char name_1[100] = "";
+	while( fgets( file_1, "%s: %s\n", other_1, name_1 ) != NULL ) {
+		if ( ! cmpstr( name_1, room_2 ) ) {
+			first_check = 1;
+			break;
 		}
 	}
 
-	fclose(file_p);
-}
-
-
-/******************************************************
- * Call this function at the beginning to create all
- * room files and add the first line to each file.
- *****************************************************/
-void makeRoomFiles( char* rooms[], char* directory ) {
-
-
-	char room_dir[100] = "";
-	FILE *file_p = NULL;
-	
-	int i;
-	for ( i = 0; i < 7; ++i ) {
-		// prepare directory string
-		strcat_safe( room_dir, 100, directory );
-		strcat_safe( room_dir, 100, "/" );
-		strcat_safe( room_dir, 100, rooms[i] );
-
-		// do initial writes to file
-		file_p = fopen( room_dir, "a+" );
-		fprintf( file_p, "ROOM NAME: %s\n", rooms[i] );
-		fclose( file_p );
-
-		memset( &room_dir[0], 0, sizeof(room_dir) );
+	// check second file for first room name
+	char other_2[100] = "";
+	char name_2[100] = "";
+	while( fgets( file_2, "%s: %s\n", other_2, name_2 ) != NULL ) {
+		if ( ! cmpstr( name_2, room_1 ) ) {
+			second_check = 1;
+			break;
+		}
 	}
+
+	// return the file pointers to end position
+	lseek( file_1, 0, SEEK_END );
+	lseek( file_2, 0, SEEK_END );
+
+	// a connection exists only if both files indicate the connection
+	return ( first_check && second_check );
 }
 
 
@@ -330,50 +387,6 @@ void addRoomTypes( char* rooms[], char* directory ) {
 
 }
 
-
-/*************************************************************************************
- * Inspired by https://randomascii.wordpress.com/2013/04/03/stopusingstrncpyalready/ 
- * Returns 1 on error or 0 on success, 1 if destination char array is too small to 
- * contain pSrc char array.
- ************************************************************************************/
-int strcat_safe(char *destination, int destCapacity, char *pSrc) {
-
-	if ( ! ( destination && pSrc ) ) {
-		printf( "in strcat_safe: null string(s) passed in!\n" );
-		exit( 1 );
-	}
-	//printf("%d\n", strlen( pSrc ) );
-
-	int capacityNeeded = strlen(destination) + strlen(pSrc) + 1; // add one for null character
-
-
-	//printf("in strcat_safe...strlen(destination) = %d \n", strlen(destination));
-	//printf("in strcat_safe...capacityNeeded = %d \n", capacityNeeded);
-
-	if (destCapacity < capacityNeeded) {
-		return 1;
-	}
-
-	strcat(destination, pSrc);
-	// ensure null termination
-	destination[strlen(destination)] = 0;
-	
-	return 0;
-}
-
-
-int main(void)
-{
-	char* rooms[] = {"blue", "red", "ONE", "TWO", "Big", "Small", "best", "worst", "CARPET", "WOOD"};
-	int SIZE = 10;
-	
-	char *directory_name;
-	setDirectory( &directory_name );
-
-	makeRooms( rooms, SIZE, directory_name );
-
-	exit(0);
-}
 
 
 
