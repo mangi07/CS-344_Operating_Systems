9fa3a4eb6ddd2ecfe84194bfaf479908bc7fecad
Benjamin Olson <olsonbe@eos-class.engr.oregonstate.edu>
2016-05-13 13:46:59 -0700

    Refactoring into getCommand()

--- assg3_smallsh/smallsh.c
+++ assg3_smallsh/smallsh.c
@@ -18,9 +18,63 @@ struct command {
 	char* file;
 	int isBackground;
 };
+typedef struct command Command;
 
 static const int OUT_FILE = 1;
 static const int IN_FILE = -1;
+
+void getCommand( Command *C ) {
+
+	const int MAXARGS = 512;
+	
+	// Initialize command struct
+	memset( &(*C), 0, sizeof( (*C) ) );
+	C->args = malloc( MAXARGS * sizeof( char ) );
+	C->argc = 0;
+
+	// Get command input into a buffer
+	char buffer[2048] = {0};
+	if ( fgets( buffer, sizeof(buffer), stdin ) == NULL ) {
+		perror( "fgets" );
+		exit( 1 );
+	}
+	int len = strlen(buffer);
+	if ( len > 0 && buffer[ len - 1 ] == '\n' ) {
+		buffer[ len - 1 ] = '\0';
+	}
+	// check whether this command is a background process
+	len = strlen(buffer);
+	if ( buffer[ len - 1 ] == '&' ) {
+		C->isBackground = 1;
+		buffer[ len - 1 ] = '\0';
+	}
+	
+	char *next_word = strtok( buffer, " " );
+	char *command = NULL;
+	if ( next_word == NULL || next_word[0] == '#' ) {
+		// return from function yet to be extracted
+		// and indicate somehow that no command was given
+	} else {
+		C->name = next_word;
+	}
+	int args_count = 0;
+	while ( next_word != NULL && args_count < MAXARGS &&
+		next_word[0] != '<' &&
+		next_word[0] != '>' ) {
+		// printf( "%s\n", next_word );
+		next_word = strtok( NULL, " " );
+		C->args[ args_count ] = next_word;
+		if ( C->args[ args_count ] != NULL ) args_count++;
+		C->argc = args_count;
+	}
+	if ( next_word != NULL && next_word == "<" ) {
+		C->redir = IN_FILE;
+	} else if ( next_word != NULL && next_word == ">" ) {
+		C->redir = OUT_FILE;
+	}
+
+}
+
 
 int main(int argc, char **argv) {
 
@@ -29,7 +83,10 @@ int main(int argc, char **argv) {
 	const int MAXARGS = 512;
 	
 	// Initialize command struct
-	struct command C;
+	Command C;
+	Command *C_ptr = &C;
+	getCommand( C_ptr );
+/*
 	memset( &C, 0, sizeof( C ) );
 	C.args = malloc( MAXARGS * sizeof( char ) );
 	C.argc = 0;
@@ -74,7 +131,7 @@ int main(int argc, char **argv) {
 	} else if ( next_word != NULL && next_word == ">" ) {
 		C.redir = OUT_FILE;
 	}
-
+*/
 	free( C.args );
 	
 	// exit cd and status
