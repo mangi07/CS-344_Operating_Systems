3517ee51fc828fd3f4bec086ef1993bd4e433691
Benjamin Olson <olsonbe@flip2.engr.oregonstate.edu>
2016-05-21 17:03:01 -0700

    Done, I think.

---
+++ assg3_smallsh/readme.txt
@@ -0,0 +1,13 @@
+Oregon State University, CS 344 - Operating Systems
+May 21, 2016
+Assignment 3: smallsh
+Student: Ben R. Olson
+
+	Instructions on compiling:
+	
+	Please place the source code file, smallsh.c, in the working directory of choice and run the following:
+	gcc -o smallsh smallsh.c
+
+
+	That's it!  Happy grading!
+
--- assg3_smallsh/smallsh.c
+++ assg3_smallsh/smallsh.c
@@ -1,6 +1,7 @@
 /************************************
  * Author: Ben R. Olson
- * Assignment: CS 344 Operating Systems Assg 3 Small sh
+ * Date: 5/21/2016
+ * Assignment: Oregon State University CS 344 Operating Systems Assg 3 smallsh
  * *********************************/
 
 #include <stdio.h>
@@ -112,47 +113,6 @@ void getCommand( Command *C ) {
 	if ( next_word != NULL && strcmp( next_word, "&" ) == 0 ) C->isBackground = 1;
 	next_word = strtok( NULL, " " );
 
-	// maybe delete the rest of this function...
-	/*
-	while ( next_word != NULL && args_count < MAXARGS ) {
-		next_word = strtok( NULL, " " );
-		if ( next_word == NULL ) break;
-		if ( next_word == ">" ) {
-			C->redir = OUT_FILE;
-			isArg = 0;
-		} else if ( next_word == "<" ) {
-			C->redir = IN_FILE;
-			isArg = 0;
-		}
-		if ( isArg ) {
-			C->args[ args_count ] = next_word;
-			args_count++;
-		} else { // it must be a file
-			C->file = next_word;
-		}
-	}
-	C->argc = args_count;
-	C->args[ args_count ] == NULL;
-
-	// check whether this command is to be a background process
-	if ( args_count > 1 && strcmp( C->args[ args_count-1 ], "&" ) == 0 ) {
-		C->isBackground = 1;
-		C->args[ args_count-1 ] = NULL;
-		C->argc--;
-	}
-	*/
-	// debug
-	/*
-	int debug_count;
-	printf( "C->name: %s\n", C->name );
-	for ( debug_count = 0; debug_count < C->argc; debug_count++ ) {
-		printf( "C->args[%d]: %s\n", debug_count, C->args[debug_count] );
-	}
-	printf( "C->argc: %d\n", C->argc );
-	printf( "C->file: %s\n", C->file );
-	printf( "C->redir: %d\n", C->redir );
-	printf( "C->isBackground: %d\n", C->isBackground );
-	*/
 }
 
 
@@ -181,8 +141,6 @@ int main(int argc, char **argv) {
 		while ( curr_b_pid != 0 /*&& inf_loop_safeguard > 0*/ ) {
 			int b_status = -5;
 			int b_w = waitpid( curr_b_pid->pid, &b_status, WNOHANG );
-			// debug
-			//printf( "checking background processes at top, pid: %d, b_status: %d\n", curr_b_pid->pid, b_status );
 			if ( b_w == -1 ) { perror("waitpid"); exit( 1 ); }
 			int isDone = 0;
 			if ( b_w == 0 ) {
@@ -204,34 +162,21 @@ int main(int argc, char **argv) {
 					b_pids_head = 0;
 					curr_b_pid = 0;
 					prev_b_pid = 0;
-					// debug
-					//printf( "removed head\n" );
 				} else if ( prev_b_pid == curr_b_pid ) { // two background processes, where curr is head
 					b_pids_head = curr_b_pid->next;
 					prev_b_pid = curr_b_pid->next;
 					free( curr_b_pid );
 					curr_b_pid = b_pids_head;
-					// debug
-					//printf( "removed head, but one more background process remains\n" );
 				} else { // at least two background processes, where curr is ahead of prev by one
 					prev_b_pid->next = curr_b_pid->next;
 					free( curr_b_pid );
 					curr_b_pid = prev_b_pid->next;
-					// debug
-					//printf( "removed curr, where at least two backgroud processes exits\n" );
 				}
 
-				// debug
-				//printf( "debug curr_b_pid: %d\n", curr_b_pid );
-				//printf( "debug prev_b_pid: %d\n", prev_b_pid );
-				//printf( "debug b_pids_head: %d\n", b_pids_head );
 			} else { // move the pid pointers
 				prev_b_pid = curr_b_pid;
 				curr_b_pid = curr_b_pid->next;
 			}
-			// debug
-			//printf( "waiting: inf_loop_safeguard: %d\n", inf_loop_safeguard );
-			//inf_loop_safeguard--;
 		}
 
 		getCommand( C_ptr );
@@ -264,22 +209,11 @@ int main(int argc, char **argv) {
 				}
 				free( dir );
 
-				// pwd to see if this worked
-				/*
-				   dir = malloc( PATH_MAX );
-				   if ( ! getcwd( dir, PATH_MAX ) ) {
-				   perror( ": getcwd" );
-				   } else {
-				   printf( "%s\n", dir );
-				   }
-				   free( dir );
-				   */
 			}
 		} else if ( C.name != NULL && strcmp( C.name, "status" ) == 0 ) { 
 			// status implemented here
 			// print out the exit status OR terminating signal of the last FOREGROUND process
 			if ( status >= 0 ) { // status has been set by the last FOREGROUND process
-				// TODO or use this example from lecture 9, p 20
 				if (WIFEXITED(status)) {
 					int exitstatus = WEXITSTATUS(status);
 					printf("exit value %d\n", exitstatus);
@@ -296,9 +230,6 @@ int main(int argc, char **argv) {
 			pid_t spawnpid = -5;
 			pid_t w_pid = -5;
 
-			// debug
-			//printf( "In the parent right before the fork\n" );
-
 			spawnpid = fork();
 
 			switch (spawnpid)
@@ -309,8 +240,6 @@ int main(int argc, char **argv) {
 					break;
 				case 0: // child, where command is executed, whether foreground or background
 					// must be a command other than cd, status, or exit
-					// debug
-					//printf( "I am the child!\n" );
 
 					/* signals */
 					if ( ! C.isBackground ) sigaction( SIGINT, &child_act, NULL );
@@ -320,7 +249,7 @@ int main(int argc, char **argv) {
 					for ( i = 0; i < C.argc; ++i ) {
 						args[i] = C.args[i];
 					}
-					// TODO In the case of background processes, 
+					// In the case of background processes, 
 					// redirect output to dev/null/ if it's not being directed to a file
 					if ( C.isBackground ) {
 						int devNull_fd = open( "/dev/null", O_RDWR );
@@ -346,8 +275,6 @@ int main(int argc, char **argv) {
 					}
 					// if condition met, redirect stdout to file given in command
 					else if ( C.redir == OUT_FILE && C.file != NULL ) {
-						// check that the arguments meet the specs
-						// TODO also, consider closing this file at the end of the outer while loop
 						int fd = open( C.file, O_WRONLY|O_CREAT|O_TRUNC, 0644 );
 						if ( fd == -1 ) {
 							// file error, so don't run the rest of command C
@@ -366,8 +293,6 @@ int main(int argc, char **argv) {
 					// if condition met, redirect stdin to file given in command, 
 					// to read in from file that which is input to execvp command
 					else if ( C.redir == IN_FILE && C.file != NULL ) {
-						// check that the arguments meet the specs
-						// also, consider closing this file at the end of the outer while loop
 						int fd = open( C.file, O_RDONLY );
 						if ( fd == -1 ) {
 							// file error, so don't run the rest of command C
@@ -389,15 +314,12 @@ int main(int argc, char **argv) {
 					return( 1 );
 					break;
 				default: // parent
-					// debug
-					//printf("In parent, switch default!\n");
 					// close-on-exec needed? ie: fctnl(fd, F_SETFD, FD_CLOEXEC) ?
 					if ( ! C.isBackground ) { 
 						// The given command is a foreground process, so wait here 
 						w_pid = waitpid( spawnpid, &status, 0 );
 						// check here for interrupt sent to parent (note: duplicated from somewhere above)
 						if ( status >= 0 ) { // status has been set by the last FOREGROUND process
-							// TODO or use this example from lecture 9, p 20
 							if ( WIFSIGNALED( status ) ) {
 								// if there was a signal caught (should be caught elsewhere), 
 								// print out the signal instead
@@ -421,8 +343,6 @@ int main(int argc, char **argv) {
 						new_b_pid->next = b_pids_head;
 						b_pids_head = new_b_pid;
 					}
-
-
 
 					break;
 			}
